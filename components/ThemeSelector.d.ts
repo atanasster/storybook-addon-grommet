bda569c2f8dc90d2dbe95a49dd7375ba34d20969
atanasster <atanasster@gmail.com>
2019-08-28 15:16:51 -0400

    clean up

--- components/ThemeSelector.d.ts
+++
@@ -1,7 +0,0 @@
-import { FunctionComponent } from 'react';
-import { Channel } from '@storybook/channels';
-interface ThemeSelectorProps {
-    channel: Channel;
-}
-export declare const ThemeSelector: FunctionComponent<ThemeSelectorProps>;
-export {};
