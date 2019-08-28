6cdbe5031ec39280372db9ca6ddc32146d296a77
atanasster <atanasster@gmail.com>
2019-08-28 14:39:43 -0400

    cleanup

--- constants.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const ADDON_ID = "storybook/grommet";
-export declare const PARAM_KEY = "grommet-theme";
-export declare const EVENTS: {
-    UPDATE: string;
-    INIT: string;
-};
--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import { BoxProps, GrommetProps } from 'grommet';
-interface withGrommetProps {
-    themes: object;
-    theme?: string;
-    boxProps?: BoxProps;
-    grommetProps?: GrommetProps;
-}
-export declare const withGrommet: ({ theme, themes, boxProps, grommetProps }: withGrommetProps) => (...args: any) => any;
-export {};
