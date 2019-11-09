b95ba112c12f255d57d215c3a4456e4c69ab8d94
atanasster <atanasster@gmail.com>
2019-11-09 08:41:37 -0800

    display theme selector in docs mode

--- src/register.tsx
+++ src/register.tsx
@@ -7,7 +7,7 @@ addons.register(ADDON_ID, api => {
   addons.add(ADDON_ID, {
     title: 'Grommet',
     type: types.TOOL,
-    match: ({ viewMode }) => viewMode === 'story',
+    match: ({ viewMode }) => viewMode === 'story' || viewMode === 'docs',
     render: () => (
       <ThemeSelector
         channel={api.getChannel()}
